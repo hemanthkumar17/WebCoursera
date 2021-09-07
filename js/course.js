fc8682c400a775cd2c695db71aaa7e7ebfceb18c
omar1024 <omar1024@protonmail.com>
2021-09-08 00:01:36 +0530

    end changes

--- js/course.js
+++ js/course.js
@@ -15,25 +15,3 @@ document.body.addEventListener("click", function (e) {
 });
 
 const sizes = ["small", "medium", "large"];
-
-function filter_search(word) {
-  word = word.toUpperCase();
-  if (w[0].toUpperCase == "S") {
-    //
-  } else if (w[0].toUpperCase == "M") {
-    //
-  } else if (w[0].toUpperCase == "L") {
-    //
-  }
-}
-
-var input = document.getElementById("search-bar");
-input.addEventListener("input", (event) => {
-  if (event.target.value === "") list = courses;
-  else list = filter_search(event.target.value);
-  let div = document.getElementById("courses");
-  div.innerHTML = "";
-  list.forEach((course) => {
-    div.appendChild(create_card(course));
-  });
-});
