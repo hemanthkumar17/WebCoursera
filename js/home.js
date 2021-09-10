3b42696d5dcbd7b199e64cc3d3f14b81989098aa
omar1024 <omar1024@protonmail.com>
2021-09-10 11:52:21 +0530

    added course logos and fixed broken links

--- js/home.js
+++ js/home.js
@@ -1,48 +1,45 @@
-const courses = ['AJAX', 'JAVA', 'JAVASCRIPT', 'HTML', 'CSS', 'PYTHON']
+const courses = ["AJAX", "JAVA", "JAVASCRIPT", "HTML", "CSS", "PYTHON"];
 
-function create_card(course){
-    let div = document.createElement('div')
-    div.className = "card"
-    // let element = document.createElement('a')
-    // element.href = '#'
-    // element.innerText = course
-    // div.appendChild(element)
-    div.style.background =  "url(src/logos/"+course+".png)";
-    div.style.backgroundSize = '150px'
-    div.id = course.toLowerCase()
-    div.addEventListener('click', (event) => {
-        window.location.href = `/${event.target.id}.html`;
-    })
-    return div
+function create_card(course) {
+  let div = document.createElement("div");
+  div.className = "card";
+  // let element = document.createElement('a')
+  // element.href = '#'
+  // element.innerText = course
+  // div.appendChild(element)
+  div.style.background = "url(src/logos/" + course + ".png)";
+  div.style.backgroundSize = "150px";
+  div.id = course.toLowerCase();
+  div.addEventListener("click", (event) => {
+    window.location.href = `${event.target.id}.html`;
+  });
+  return div;
 }
 
-function filter_search(word){
-    word = word.toUpperCase()
-    let len = word.length
-    let list = []
-    courses.forEach(course => {
-        if(course.substring(0,len)===word)
-            list.push(course)
-    }); 
-    return list
+function filter_search(word) {
+  word = word.toUpperCase();
+  let len = word.length;
+  let list = [];
+  courses.forEach((course) => {
+    if (course.substring(0, len) === word) list.push(course);
+  });
+  return list;
 }
 
-var input = document.getElementById('search-bar')
-input.addEventListener('input', (event) => {
-    let list;
-    if(event.target.value==='')
-        list = courses
-    else
-        list = filter_search(event.target.value)
-    let div = document.getElementById('courses')
-    div.innerHTML = ''
-    list.forEach(course => {
-        div.appendChild(create_card(course))
-    });
-})
+var input = document.getElementById("search-bar");
+input.addEventListener("input", (event) => {
+  let list;
+  if (event.target.value === "") list = courses;
+  else list = filter_search(event.target.value);
+  let div = document.getElementById("courses");
+  div.innerHTML = "";
+  list.forEach((course) => {
+    div.appendChild(create_card(course));
+  });
+});
 
-let div = document.getElementById('courses')
-div.innerHTML = ''
-courses.forEach(course => {
-    div.appendChild(create_card(course))
+let div = document.getElementById("courses");
+div.innerHTML = "";
+courses.forEach((course) => {
+  div.appendChild(create_card(course));
 });
