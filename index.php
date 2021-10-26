132f4be124924eb4cd13b4f9be6018d05b522143
omar1024 <omar1024@protonmail.com>
2021-10-26 15:57:11 +0530

    login and pwd not fixed

---
+++ index.php
@@ -0,0 +1,76 @@
+<?php
+
+  $email = '';
+  $password = '';
+  $errors = array();
+  $db = mysqli_connect('localhost','root','','user_info');
+  if(isset($_POST['submit'])){
+    $email = mysqli_real_escape_string($db, $_POST['emailId']);
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+
+
+    if(empty($email)){
+      array_push($errors, "Email is required");
+    }
+    if(empty($password)){
+      array_push($errors, "Password is required");
+    }
+
+    if(count($errors)==0){
+      $sql = "SELECT password FROM user_login WHERE email='$email'";
+      $result=mysqli_query($db,$sql);
+      echo mysql_result($result);
+     
+    }
+  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Course Exercise" />
+    <meta name="author" content="Emmanuel, omar1024, hemanthkumar" />
+    <title>Coursera</title>
+    <link rel="icon" href="src/icon.png" />
+    <link rel="stylesheet" href="css/login.css" />
+    <!-- CSS only -->
+  
+  </head>
+  <body>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>  
+    <form class="form2" method="post" action="courseList.html">
+      
+      <div class="title">WebCoursera</div>
+      <div class="subtitle">Login</div>
+      <?php if (count($errors)>0): 
+        foreach($errors as $error):?>
+        <div class="alert alert-danger" role="alert">
+          <?php echo $error; ?>
+        </div>
+        <?php 
+          endforeach;
+        endif; ?>
+      <div class="input-container ic12">
+        <input id="emailId" name="emailId" class="input" type="email" placeholder=" " />
+        <div class="cut"></div>
+        <label for="emailId" class="placeholder">Email Id</label>
+      </div>
+      <div class="input-container ic2">
+        <input id="password" name="password" class="input" type="password" placeholder=" " />
+        <div class="cut"></div>
+        <label for="password" class="placeholder">Password</label>
+      </div>
+      <button name="submit" type="submit" class="button">Log In</button>
+     
+    </form>
+
+    <!-- <script type="text/javascript">
+      document.getElementById("submit").onclick = function () {
+        location.href = "../index.html";
+      };
+    </script> -->
+  </body>
+</html>
