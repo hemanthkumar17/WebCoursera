d5ba7a3176439d95d72d36312f04af4a3f22689d
EmmanuelMarianMat <emmanuelmarianmat@gmail.com>
2021-10-27 20:11:06 +0530

    small change in stats.php

--- pages/statistic.php
+++ pages/statistic.php
@@ -37,7 +37,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/stats.css">
+    <link rel="stylesheet" href="../css/stats.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://use.fontawesome.com/875f2af2ec.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
